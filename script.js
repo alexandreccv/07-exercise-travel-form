fcd135a9eecefd8f96c7ed4c44ff6c90c62b3e17
Alexandre Carvalho <alex380bb@gmail.com>
2023-07-31 22:07:13 -0300

    refatorando

--- script.js
+++ script.js
@@ -17,3 +17,10 @@ window.onload = function () {
   const clearBtn = document.querySelector('#clear-btn');
   clearBtn.addEventListener('click', clearFields);
 };
+
+submitBtn.disabled = true;
+const agreementCheckbox = document.getElementById('agreement');
+
+agreementCheckbox.addEventListener('change', () => {
+  submitBtn.disabled = !agreementCheckbox.checked;
+});
